f19046f9cc03a9549a6aee2b7ea5e1f0174791b4
Cosimo Leone <cosi.leone@icloud.com>
2016-11-29 18:10:35 -0500

    whitespace waaaaaaaars

--- Python/spreadsheet_editor.py
+++ Python/spreadsheet_editor.py
@@ -11,10 +11,10 @@ from TimeIsMoneyFriend import AuctionHouse
 
 
 try:
-	import argparse
-	flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
-	flags = None
+    flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
@@ -31,61 +31,61 @@ class SheetsSession:
         self.spreadsheetId = spreadsheetId
 
 def get_credentials():
-	"""Gets valid user credentials from storage.
+    """Gets valid user credentials from storage.
 
-	If nothing has been stored, or if the stored credentials are invalid,
-	the OAuth2 flow is completed to obtain the new credentials.
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
 
-	Returns:
-		Credentials, the obtained credential.
-	"""
-	home_dir = os.path.expanduser('~')
-	credential_dir = os.path.join(home_dir, '.credentials')
-	if not os.path.exists(credential_dir):
-		os.makedirs(credential_dir)
-	credential_path = os.path.join(credential_dir,
-								   'sheets.googleapis.com-python-quickstart.json')
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
 
-	store = Storage(credential_path)
-	credentials = store.get()
-	if not credentials or credentials.invalid:
-		flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-		flow.user_agent = APPLICATION_NAME
-		if flags:
-			credentials = tools.run_flow(flow, store, flags)
-		else: # Needed only for compatibility with Python 2.6
-			credentials = tools.run(flow, store)
-		print('Storing credentials to ' + credential_path)
-	return credentials
-	
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
 def getAllItemIds(sheetsSession):
 
-	rangeName = 'Manual Info!A2:A'
-	result = sheetsSession.service.spreadsheets().values().get(
-		spreadsheetId=sheetsSession.spreadsheetId, range=rangeName).execute()
-	values = result.get('values', [])
+    rangeName = 'Manual Info!A2:A'
+    result = sheetsSession.service.spreadsheets().values().get(
+        spreadsheetId=sheetsSession.spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
 
-	shopping_list = []
-	ah = AuctionHouse()
+    shopping_list = []
+    ah = AuctionHouse()
 
-	for row in values:
-		item_id = ah.get_item_id(row[0])
-		shopping_list.append((row[0], item_id, None))
-		if item_id != -1:
-			print("%i %s" % (item_id, row[0]))
-			
+    for row in values:
+        item_id = ah.get_item_id(row[0])
+        shopping_list.append((row[0], item_id, None))
+        if item_id != -1:
+            print("%i %s" % (item_id, row[0]))
+
 def writeData(sheetsSession):
-	rangeName = 'phptest'
-	values = [
-	["time","is","money","friend!","",""],
-	[7,8,9,10,11,12]
-	]
-	body = {
-	  'values': values
-	}
-	result = sheetsSession.service.spreadsheets().values().update(
-		spreadsheetId=sheetsSession.spreadsheetId, range=rangeName,
-		valueInputOption='USER_ENTERED', body=body).execute()
+    rangeName = 'phptest'
+    values = [
+        ["time","is","money","friend!","",""],
+        [7,8,9,10,11,12]
+    ]
+    body = {
+        'values': values
+    }
+    result = sheetsSession.service.spreadsheets().values().update(
+        spreadsheetId=sheetsSession.spreadsheetId, range=rangeName,
+        valueInputOption='USER_ENTERED', body=body).execute()
 
 def main():
     """ 'Time is Money, Friend!' spreadsheet URL:
@@ -96,19 +96,19 @@ def main():
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
     service = discovery.build('sheets', 'v4', http=http,
-                            discoveryServiceUrl=discoveryUrl)
+                              discoveryServiceUrl=discoveryUrl)
 
     spreadsheetId = '1nxeLFMeKLHu-EtHpYz4XL0woNEkyK3coXHZ6CSQV9UA'
-    
+
     sheetsSession = SheetsSession(credentials, http, discoveryUrl, service, spreadsheetId)
 
     # WARNING: slow operation
     print("fetching ALL AH data now! (feel free to interrupt)")
     #data = ah.get_whole_ah()
     filtered_ah = []
-    
+
     getAllItemIds(sheetsSession)
     writeData(sheetsSession)
 
 if __name__ == '__main__':
-	main()
+    main()
