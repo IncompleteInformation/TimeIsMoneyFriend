322ac052779b2ce32c3a9f4693fc4eb134c871b0
cosileone <cosi.leone@icloud.com>
2016-12-28 15:26:12 +0000

    Add link to update data add some timing functions

--- Python/web_app.py
+++ Python/web_app.py
@@ -1,18 +1,29 @@
 from flask import Flask
 from datetime import datetime
 import spreadsheet_editor as se
+import TimeIsMoneyFriend
+from TimeIsMoneyFriend import AuctionHouse
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Append /update to URL to fetch AH data."
+    return 'Click <a href="/update">here</a> to fetch AH data.'
 
 
 @app.route("/update")
 def fetch_data():
     se.main()
     return "Updated Spreadsheet at " + str(datetime.now())[:-7]
+
+
+# @app.route("/<server_slug>")
+# def set_server(server_slug):
+
+
+# @app.route("/item/<item_id>")
+# def fetch_item_name(item_id):
+#     return TimeIsMoneyFriend.item_db
 
 if __name__ == "__main__":
     host = "127.0.0.1"
